c54740e9975f69fbb1a5275e8445fedb15b66be7
richarquispe <85771167+richarquispe@users.noreply.github.com>
2024-05-12 10:18:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.all("*", (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
+if (process.env.ENVIRONMENT === "serverless") {
+    module.exports.handler = serverless(app);
+} else {
+    app.listen(PORT, () => {
+        console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    });
+}
